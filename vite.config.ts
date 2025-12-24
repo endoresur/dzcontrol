7bd12e9de6333d6097c4cc6b733c377b733badbe
 Николай <mlhv@MacBook-Air-Nikolaj.local>
2025-12-25 02:18:15 +0500

    comment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dzcontrol/",
+  // base: "/dzcontrol/",
   optimizeDeps: {
     include: ["react/jsx-runtime"],
   },
